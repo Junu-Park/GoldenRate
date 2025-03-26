25ea4641aff49d42616a21657505af961b2c87b7
Park Junwoo <ays99111@naver.com>
2025-03-26 15:32:08 +0900

    font setting
    Pretendard 폰트 설정

---
+++ GoldenRate/GoldenRate/Resource/Font/+UIFont.swift
@@ -0,0 +1,25 @@
+//
+//  +UIFont.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 3/26/25.
+//
+
+import UIKit
+
+extension UIFont {
+    static let bold14: UIFont = .init(name: "Pretendard-Bold", size: 14)!
+    static let bold16: UIFont = .init(name: "Pretendard-Bold", size: 16)!
+    static let bold18: UIFont = .init(name: "Pretendard-Bold", size: 18)!
+    static let bold20: UIFont = .init(name: "Pretendard-Bold", size: 20)!
+    
+    static let regular14: UIFont = .init(name: "Pretendard-Regular", size: 14)!
+    static let regular16: UIFont = .init(name: "Pretendard-Regular", size: 16)!
+    static let regular18: UIFont = .init(name: "Pretendard-Regular", size: 18)!
+    static let regular20: UIFont = .init(name: "Pretendard-Regular", size: 20)!
+    
+    static let thin14: UIFont = .init(name: "Pretendard-Thin", size: 14)!
+    static let thin16: UIFont = .init(name: "Pretendard-Thin", size: 16)!
+    static let thin18: UIFont = .init(name: "Pretendard-Thin", size: 18)!
+    static let thin20: UIFont = .init(name: "Pretendard-Thin", size: 20)!
+}
