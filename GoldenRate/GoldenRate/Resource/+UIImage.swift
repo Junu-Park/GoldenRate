4777133e1de2abac00f29ef8c24827835d0c4117
Park Junwoo <ays99111@naver.com>
2025-03-27 20:53:07 +0900

    symbol uiimage type property add
    탭 바 아이템에 사용할 심볼 이미지 타입 프로퍼티 추가

---
+++ GoldenRate/GoldenRate/Resource/+UIImage.swift
@@ -0,0 +1,15 @@
+//
+//  +UIImage.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 3/26/25.
+//
+
+import UIKit
+
+extension UIImage {
+    static let home = UIImage(systemName: "house")!
+    static let search = UIImage(systemName: "magnifyingglass")!
+    static let percent = UIImage(systemName: "percent")!
+    static let more = UIImage(systemName: "ellipsis")!
+}
