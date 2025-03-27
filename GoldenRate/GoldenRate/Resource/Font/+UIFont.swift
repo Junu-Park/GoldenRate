c82a2ee73f3225d6f0c49ad395165f014c1e6afa
Park Junwoo <ays99111@naver.com>
2025-03-27 19:09:52 +0900

    font size add
    10 size 폰트 추가

--- GoldenRate/GoldenRate/Resource/Font/+UIFont.swift
+++ GoldenRate/GoldenRate/Resource/Font/+UIFont.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 extension UIFont {
+    static let bold10: UIFont = .init(name: "Pretendard-Bold", size: 10)!
     static let bold14: UIFont = .init(name: "Pretendard-Bold", size: 14)!
     static let bold16: UIFont = .init(name: "Pretendard-Bold", size: 16)!
     static let bold18: UIFont = .init(name: "Pretendard-Bold", size: 18)!
     static let bold20: UIFont = .init(name: "Pretendard-Bold", size: 20)!
     
+    static let regular10: UIFont = .init(name: "Pretendard-Regular", size: 10)!
     static let regular14: UIFont = .init(name: "Pretendard-Regular", size: 14)!
     static let regular16: UIFont = .init(name: "Pretendard-Regular", size: 16)!
     static let regular18: UIFont = .init(name: "Pretendard-Regular", size: 18)!
     static let regular20: UIFont = .init(name: "Pretendard-Regular", size: 20)!
     
+    static let thin10: UIFont = .init(name: "Pretendard-Thin", size: 10)!
     static let thin14: UIFont = .init(name: "Pretendard-Thin", size: 14)!
     static let thin16: UIFont = .init(name: "Pretendard-Thin", size: 16)!
     static let thin18: UIFont = .init(name: "Pretendard-Thin", size: 18)!
