b2090be37b2ea21a3ef2c94acd8425a8f5ce8b4e
Park Junwoo <ays99111@naver.com>
2025-03-28 14:55:53 +0900

    RateChartEntity model create
    Chart에서 사용할 RateChartEntity 모델 생성

---
+++ GoldenRate/GoldenRate/Model/Entity/RateChartEntity.swift
@@ -0,0 +1,14 @@
+//
+//  RateChartEntity.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 3/28/25.
+//
+
+import Foundation
+
+struct RateChartEntity {
+    let date: Date
+    let rate: Double
+    let type: RateType
+}
