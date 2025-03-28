786348b673f5d1530d17edf81c3a7f4e7c74a607
Park Junwoo <ays99111@naver.com>
2025-03-28 16:30:10 +0900

    Font Extension create
    SwiftUI에서 사용할 커스텀 폰트 Extension 생성

---
+++ GoldenRate/GoldenRate/Resource/Font/+Font.swift
@@ -0,0 +1,14 @@
+//
+//  +Font.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/7/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let bold10: Font = .custom("Pretendard-Bold", size: 10)
+    
+    static let regular10: Font = .custom("Pretendard-Regular", size: 10)
+}
