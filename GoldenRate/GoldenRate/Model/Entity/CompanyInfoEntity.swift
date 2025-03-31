675c018435112296a84c630de22b50cc21dae684
Park Junwoo <ays99111@naver.com>
2025-03-31 17:22:17 +0900

    CompanyInfoEntity create
    은행 정보 Entity 생성

---
+++ GoldenRate/GoldenRate/Model/Entity/CompanyInfoEntity.swift
@@ -0,0 +1,13 @@
+//
+//  CompanyInfoEntity.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 3/31/25.
+//
+
+import Foundation
+
+struct CompanyInfoEntity {
+    let id: String
+    let name: String
+}
