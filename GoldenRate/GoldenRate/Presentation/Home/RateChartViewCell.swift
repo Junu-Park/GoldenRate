cbd7a2b5066f0cc9784de3a8f05ed6d0735324b6
Park Junwoo <ays99111@naver.com>
2025-04-02 15:51:29 +0900

    RateChartViewCell create
    HomeTab에서 금리차트를 보여주는 Custom Cell 생성

---
+++ GoldenRate/GoldenRate/Presentation/Home/RateChartViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  RateChartViewCell.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/1/25.
+//
+
+import SwiftUI
+import UIKit
+
+import SnapKit
+
+final class RateChartViewCell: BaseCollectionViewCell {
+    
+    private let titleLabel: UILabel = UILabel()
+    private let subtitleLabel: UILabel = UILabel()
+    private let rateChartViewController: UIHostingController<RateChartView> = UIHostingController(rootView: .init(rateDataList: []))
+    
+    override func configureHierarchy() {
+        self.contentView.addSubviews(self.titleLabel, self.subtitleLabel, self.rateChartViewController.view)
+    }
+    
+    override func configureLayout() {
+        self.titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        self.subtitleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        self.subtitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(4)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        self.rateChartViewController.view.snp.makeConstraints {
+            $0.top.equalTo(self.subtitleLabel.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        self.contentView.backgroundColor = .defaultBackground
+        self.contentView.layer.cornerRadius = 15
+        self.contentView.clipsToBounds = true
+        
+        self.layer.shadowColor = UIColor.defaultText.cgColor
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = .zero
+        
+        let titleLabelString = NSAttributedString(totalString: "금리 차트", totalColor: .defaultText, totalFont: .bold18, targetString: "금리", targetColor: .accent, targetFont: .bold18)
+        self.titleLabel.attributedText = titleLabelString
+        
+        self.subtitleLabel.text = "예금금리: 정기예금 12개월 기준"
+        self.subtitleLabel.font = .regular14
+        self.subtitleLabel.textColor = .defaultGray
+        
+        self.rateChartViewController.view.backgroundColor = .clear
+    }
+    
+    func setView(data: [RateChartEntity]) {
+        self.rateChartViewController.rootView = .init(rateDataList: data)
+    }
+}
