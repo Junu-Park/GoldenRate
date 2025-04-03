b8a58a91ff18bbeff97b1755e865713e52fc992a
Park Junwoo <ays99111@naver.com>
2025-04-03 23:07:59 +0900

    InterestRateType create
    이율 계산 타입 enum 추가

---
+++ GoldenRate/GoldenRate/Model/Enum/InterestRateType.swift
@@ -0,0 +1,14 @@
+//
+//  InterestRateType.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/2/25.
+//
+
+import Foundation
+
+enum InterestRateType: String, CaseIterable {
+    case all = "전체"
+    case simple = "단리"
+    case compound = "복리"
+}
