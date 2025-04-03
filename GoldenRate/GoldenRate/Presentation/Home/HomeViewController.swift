0993c787c77ff12d4e1c94c165dbe914a88cb90e
Park Junwoo <ays99111@naver.com>
2025-04-03 19:38:17 +0900

    HomeViewController implement
    HomeTab VC 구현

--- GoldenRate/GoldenRate/Presentation/Home/HomeViewController.swift
+++ GoldenRate/GoldenRate/Presentation/Home/HomeViewController.swift
@@ -13,7 +13,8 @@ import SnapKit
 
 final class HomeViewController: BaseViewController {
 
-    private let chartViewController: UIHostingController<RateChartView> = UIHostingController(rootView: .init())
+    private lazy var homeCollectionViewDataSource: UICollectionViewDiffableDataSource<HomeCollectionViewSection, HomeCollectionViewItem> = self.configureCollectionViewDataSource()
+    private lazy var homeCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.configureCollectionViewLayout())
     
     private let viewModel: HomeViewModel
     private var cancellable: Set<AnyCancellable> = Set<AnyCancellable>()
@@ -28,51 +29,113 @@ final class HomeViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        self.addChild(self.chartViewController)
-        self.view.addSubview(self.chartViewController.view)
-        self.chartViewController.didMove(toParent: self)
+        self.view.addSubview(self.homeCollectionView)
     }
     
     override func configureLayout() {
-        self.chartViewController.view.snp.makeConstraints {
-            $0.edges.equalTo(self.view.safeAreaLayoutGuide).inset(32)
+        self.homeCollectionView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
     }
     
     override func configureView() {
-        self.chartViewController.view.backgroundColor = .clear
+        self.homeCollectionView.backgroundColor = .clear
+        self.homeCollectionView.contentInset = .init(top: 0, left: 0, bottom: 16, right: 0)
+        self.homeCollectionView.register(RateChartViewCell.self, forCellWithReuseIdentifier: RateChartViewCell.identifier)
+        self.homeCollectionView.register(RateRankingViewCell.self, forCellWithReuseIdentifier: RateRankingViewCell.identifier)
     }
     
     override func bind() {
-        
         let getRateChartData = CurrentValueSubject<Void, Never>(())
         let getDepositProductTopData = CurrentValueSubject<Void, Never>(())
         let getSavingProductTopData = CurrentValueSubject<Void, Never>(())
         
         let input = HomeViewModel.Input(getRateChartData: getRateChartData.eraseToAnyPublisher(), getDepositProductTopData: getDepositProductTopData.eraseToAnyPublisher(), getSavingProductTopData: getSavingProductTopData.eraseToAnyPublisher())
-        
         let output = self.viewModel.tranform(input: input)
         
-        output.rateChartData
-            .sink { [weak self] data in
-                self?.chartViewController.rootView = .init(rateDataList: data)
+        Publishers.CombineLatest3(
+                output.rateChartData,
+                output.depositProductTopData,
+                output.savingProductTopData
+            )
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (chartData, depositProducts, savingProducts) in
+                guard let self else {
+                    return
+                }
+                
+                self.updateSnapshot(chartData: chartData, depositData: depositProducts, savingData: savingProducts)
             }
             .store(in: &self.cancellable)
+    }
+}
+
+extension HomeViewController {
+    private func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
         
-        output.depositProductTopData
-            .sink { [weak self] data in
-                print("-------")
-                print(data)
-                print("-------")
-            }
-            .store(in: &self.cancellable)
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 16
         
-        output.savingProductTopData
-            .sink { [weak self] data in
-                print("-------")
-                print(data)
-                print("-------")
+        let layout = UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, env in
+            
+            let itemHeight: NSCollectionLayoutDimension
+            let groupHeight: NSCollectionLayoutDimension
+            
+            if sectionIndex == 0 {
+                itemHeight = .fractionalHeight(1)
+                groupHeight = .fractionalHeight(0.5)
+            } else {
+                itemHeight = .estimated(400)
+                groupHeight = .estimated(400)
             }
-            .store(in: &self.cancellable)
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: itemHeight)
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+            
+            return section
+        }, configuration: config)
+        return layout
+    }
+    
+    private func configureCollectionViewDataSource() -> UICollectionViewDiffableDataSource<HomeCollectionViewSection, HomeCollectionViewItem> {
+        return .init(collectionView: self.homeCollectionView) { collectionView, indexPath, itemType in
+            switch itemType {
+            case .chart(let item):
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RateChartViewCell.identifier, for: indexPath) as? RateChartViewCell else {
+                    return UICollectionViewCell()
+                }
+                cell.setView(data: item)
+                return cell
+            case .ranking(let depositItem, let savingItem):
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RateRankingViewCell.identifier, for: indexPath) as? RateRankingViewCell else {
+                    return UICollectionViewCell()
+                }
+                cell.setView(depositProducts: depositItem)
+                cell.setView(savingsProducts: savingItem)
+
+                return cell
+            }
+        }
+    }
+    
+    private func updateSnapshot(chartData: [RateChartEntity], depositData: [DepositProductEntity], savingData: [SavingProductEntity]) {
+        var snapshot = NSDiffableDataSourceSnapshot<HomeCollectionViewSection, HomeCollectionViewItem>()
+        
+        snapshot.appendSections([.chart, .ranking])
+        
+        let chartItem = HomeCollectionViewItem.chart(item: chartData)
+        let rankingItem = HomeCollectionViewItem.ranking(depositItem: depositData, savingItem: savingData)
+        
+        snapshot.appendItems([chartItem], toSection: .chart)
+        snapshot.appendItems([rankingItem], toSection: .ranking)
+        
+        self.homeCollectionViewDataSource.apply(snapshot, animatingDifferences: true)
     }
 }
