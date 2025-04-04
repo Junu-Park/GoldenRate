d33d3eda3d1b9af63b32e8332b32032c374ceaa9
Park Junwoo <ays99111@naver.com>
2025-04-04 16:30:43 +0900

    ProductSortType create
    상품 정렬 방식 관리 타입 생성

---
+++ GoldenRate/GoldenRate/Model/Enum/ProductSortType.swift
@@ -0,0 +1,13 @@
+//
+//  ProductSortType.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/4/25.
+//
+
+import Foundation
+
+enum ProductSortType: String, CaseIterable {
+    case basic = "기본순"
+    case highestRate = "최고금리순"
+}
