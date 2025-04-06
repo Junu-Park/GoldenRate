da891648b43f8d848edb00dedfc6ff159460ca52
Park Junwoo <ays99111@naver.com>
2025-04-06 20:30:16 +0900

    PeriodType create
    상품 시간을 관리하는 열거형 생성

---
+++ GoldenRate/GoldenRate/Model/Enum/PeriodType.swift
@@ -0,0 +1,13 @@
+//
+//  PeriodType.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/6/25.
+//
+
+import Foundation
+
+enum PeriodType: String, CaseIterable {
+    case month = "개월"
+    case year = "년"
+}
