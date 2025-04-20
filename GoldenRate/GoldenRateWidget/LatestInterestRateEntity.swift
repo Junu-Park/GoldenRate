e002fea19d47fc27e9ff98a2416adebaf252522b
Park Junwoo <ays99111@naver.com>
2025-04-20 17:50:35 +0900

    LatestInterestRateEntity
    위젯에서 사용할 최근 금리 데이터에 대한 Entity 모델 구현

---
+++ GoldenRate/GoldenRateWidget/LatestInterestRateEntity.swift
@@ -0,0 +1,55 @@
+//
+//  LatestInterestRateEntity.swift
+//  GoldenRate
+//
+//  Created by 박준우 on 4/20/25.
+//
+
+import SwiftUI
+
+struct LatestInterestRateEntity {
+    let rateType: LatestRateType
+    let previousRate: Double
+    let currentRate: Double
+    
+    /// 이전 금리에서 현재 금리 변화상태
+    ///  -1: 하락, 0: 변화 없음. 1: 상승
+    private func changeState() -> Int {
+        switch self.currentRate - self.previousRate {
+        case ..<0:
+            return -1
+        case 0:
+            return 0
+        default:
+            return 1
+        }
+    }
+    
+    func changeColor() -> Color {
+        switch self.changeState() {
+        case -1:
+            return .blue
+        case 1:
+            return .red
+        default:
+            return .defaultGray
+        }
+    }
+    
+    func changeSymbol() -> String {
+        switch self.changeState() {
+        case -1:
+            return "▼"
+        case 1:
+            return "▲"
+        default:
+            return "＝"
+        }
+    }
+    
+    static let mock: [LatestInterestRateEntity] = [
+        LatestInterestRateEntity(rateType: .base, previousRate: 2.75, currentRate: 2.75),
+        LatestInterestRateEntity(rateType: .first, previousRate: 3.06, currentRate: 2.98),
+        LatestInterestRateEntity(rateType: .second, previousRate: 3.30, currentRate: 3.10)
+    ]
+}
