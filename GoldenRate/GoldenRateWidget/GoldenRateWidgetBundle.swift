20af0fc8b423d67fe333d41371a2ca38d275a10f
Park Junwoo <ays99111@naver.com>
2025-04-20 12:17:49 +0900

    Widget Target add
    위젯 추가를 위한 위젯 타겟 추가

---
+++ GoldenRate/GoldenRateWidget/GoldenRateWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GoldenRateWidgetBundle.swift
+//  GoldenRateWidget
+//
+//  Created by 박준우 on 4/20/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GoldenRateWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GoldenRateWidget()
+    }
+}
