ab241f3f8b90288caf91c9822d4b185766d11653
Park Junwoo <ays99111@naver.com>
2025-07-10 15:04:10 +0900

    NSLayoutYAxisAnchor/NSLayoutXAxisAnchor 배열의 constraints 메서드 오버로딩

--- GoldenRate/GoldenRate/Extension/+Array.swift
+++ GoldenRate/GoldenRate/Extension/+Array.swift
@@ -15,6 +15,14 @@ extension Array where Element == NSLayoutYAxisAnchor {
             self[1].constraint(equalTo: view.bottomAnchor, constant: -constant)
         ]
     }
+    
+    func constraints(equalTo safeArea: UILayoutGuide?, constant: CGFloat = 0) -> [NSLayoutConstraint] {
+        guard let safeArea else { return [] }
+        return [
+            self[0].constraint(equalTo: safeArea.topAnchor, constant: constant),
+            self[1].constraint(equalTo: safeArea.bottomAnchor, constant: -constant)
+        ]
+    }
 }
 
 extension Array where Element == NSLayoutXAxisAnchor {
@@ -24,6 +32,14 @@ extension Array where Element == NSLayoutXAxisAnchor {
         return [
             self[0].constraint(equalTo: view.leadingAnchor, constant: constant),
             self[1].constraint(equalTo: view.trailingAnchor, constant: -constant)
+        ]
+    }
+    
+    func constraints(equalTo safeArea: UILayoutGuide?, constant: CGFloat = 0) -> [NSLayoutConstraint] {
+        guard let safeArea else { return [] }
+        return [
+            self[0].constraint(equalTo: safeArea.leadingAnchor, constant: constant),
+            self[1].constraint(equalTo: safeArea.trailingAnchor, constant: -constant)
         ]
     }
 }
